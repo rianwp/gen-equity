feb5bf1333faef183fda627b3cbc3fad2ae3282b
rianwp <rianwp2015@gmail.com>
2022-08-24 12:22:30 +0700

    Create index.jsx

---
+++ components/Hero/index.jsx
@@ -0,0 +1,13 @@
+import BackgroundHero from "./BackgroundHero"
+import HeroContent from "./HeroContent"
+
+const Hero = () => {
+  return (
+    <>
+      <HeroContent/>
+      <BackgroundHero/>
+    </>
+  )
+}
+
+export default Hero
