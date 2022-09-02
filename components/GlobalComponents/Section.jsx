f4affc381758e4b254b615fe96a90dac8f91c578
rianwp <rianwp2015@gmail.com>
2022-09-03 00:10:23 +0700

    Create Section.jsx

---
+++ components/GlobalComponents/Section.jsx
@@ -0,0 +1,9 @@
+const Section = ({children}) => {
+  return (
+    <div className="relative w-full lg:h-[850px] md:h-[700px] h-[400px]">
+      {children}
+    </div>
+  )
+}
+
+export default Section
