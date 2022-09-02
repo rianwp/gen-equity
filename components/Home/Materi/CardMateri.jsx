81d9e5fee90d32087090062f3fd35c2a98a194b8
rianwp <rianwp2015@gmail.com>
2022-09-03 00:10:53 +0700

    Create CardMateri.jsx

---
+++ components/Home/Materi/CardMateri.jsx
@@ -0,0 +1,25 @@
+import { motion } from "framer-motion"
+
+const CardMateri = ({children}) => {
+  const cardAnimation ={
+    visible: { opacity: 1, scaleY: [0.8, 1], scaleX: 1, transition: { duration: 0.3} },
+    hidden: { opacity: 0, scaleX: 0.1, scaleY: 0.1 },
+    hover: {scale: 1.1}
+  }
+  return (
+    <motion.div
+      variants={cardAnimation}
+      initial="hidden"
+      whileInView="visible"
+      whileHover="hover"
+      className="shrink-0 lg:w-[300px] lg:h-[400px] md:w-[250px] md:h-[300px] w-[150px] h-[200px] bg-gradient-to-b from-[#0031FF] to-[#B900FF] p-1 rounded-2xl lg:m-8 md:m-4 m-2 relative"
+    >
+      <div className="z-0 absolute top-0 bg-black/90 w-full h-full blur-md"></div>
+      <div className="w-full h-full">
+        {children}
+      </div>
+    </motion.div>
+  )
+}
+
+export default CardMateri
