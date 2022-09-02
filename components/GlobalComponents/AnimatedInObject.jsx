cbf1974c774f2f1011c380dafa6fd5e0f6e0eb3e
rianwp <rianwp2015@gmail.com>
2022-09-03 00:10:17 +0700

    Update AnimatedInObject.jsx

--- components/GlobalComponents/AnimatedInObject.jsx
+++ components/GlobalComponents/AnimatedInObject.jsx
@@ -1,29 +1,11 @@
-import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-import { useEffect } from "react";
+import { motion } from "framer-motion"
 
-const AnimatedInObject = ({children, className, distance}) => {
-  const objectVariant = {
-    visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
-    hidden: { opacity: 0, x: distance }
-  }
-  const control = useAnimation()
-  const [ref, inView] = useInView({})
-
-  useEffect(() => {
-    if (inView) {
-      control.start("visible")
-    } else {
-      control.start("hidden")
-    }
-  }, [control, inView])
-
+const AnimatedInObject = ({children, className, animation}) => {
   return (
     <motion.div
-      ref={ref}
-      variants={objectVariant}
+      variants={animation}
       initial="hidden"
-      animate={control}
+      whileInView="visible"
       className={className}
     >
       {children}
