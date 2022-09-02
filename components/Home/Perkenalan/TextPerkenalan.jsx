afb4d96622a3abc00be6107391b1665f2ae906a2
rianwp <rianwp2015@gmail.com>
2022-09-03 00:11:12 +0700

    Create TextPerkenalan.jsx

---
+++ components/Home/Perkenalan/TextPerkenalan.jsx
@@ -0,0 +1,16 @@
+import Container from "../../GlobalComponents/Container"
+import AnimatedInObject from "../../GlobalComponents/AnimatedInObject"
+
+const TextPerkenalan = () => {
+  const perkenalanAnimation = {
+    visible: { opacity: 1, transition: { duration: 0.5 } },
+    hidden: { opacity: 0 }
+  }
+  return (
+    <Container className="lg:w-1/2 w-full px-2">
+      <AnimatedInObject animation={perkenalanAnimation} className="text-white font-poppins lg:text-2xl md:text-xl text-sm lg:mt-8 mt-4">Halo sobat GE, selamat datang di web GenEquity. Tenang jangan takut, aku tidak menyengat kamu kok, hehe. Kenalin dulu namaku Lee, si lebah cerdas yang akan menemani kamu dalam menjelaskan banyak materi dan survey tentang Gender Equality nih. Yuk ikutin perjalananku untuk lebih tau tentang Gender Equality 😁.</AnimatedInObject>
+    </Container>
+  )
+}
+
+export default TextPerkenalan
