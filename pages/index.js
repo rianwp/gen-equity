9a0cb481d404eeaca728fc18617200d31ae91d01
rianwp <rianwp2015@gmail.com>
2022-09-03 00:11:27 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
-import Hero from "../components/Hero"
-import Materi from "../components/Materi"
 import Navbar from "../components/Navbar"
+import Hero from "../components/Home/Hero"
+import Perkenalan from "../components/Home/Perkenalan"
+import Materi from "../components/Home/Materi"
+import Container from "../components/GlobalComponents/Container"
 
 const Home = () => {
   return (
     <>
       <Navbar/>
-      <Hero/>
-      <Materi/>
+      <div className="bg-home bg-cover bg-top bg-no-repeat overflow-hidden">
+        <Container>
+          <Hero/>
+          <Perkenalan/>
+          <Materi/>
+        </Container>
+      </div>
     </>
     
   )
