95ac6282308ebd5b8f3b8ca60c2bc38b137959ab
rianwp <rianwp2015@gmail.com>
2022-09-06 20:59:48 +0700

    Update Section.jsx

--- components/GlobalComponents/Section.jsx
+++ components/GlobalComponents/Section.jsx
@@ -1,6 +1,6 @@
 const Section = ({children}) => {
   return (
-    <div className="relative w-full lg:h-[850px] md:h-[700px] h-[400px]">
+    <div className="relative w-full lg:h-[850px] md:h-[700px] h-[600px]">
       {children}
     </div>
   )
