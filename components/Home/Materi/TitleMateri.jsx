4215153f6a76d19c3f1920bd3a83301eef75cc00
rianwp <rianwp2015@gmail.com>
2022-09-06 21:00:30 +0700

    Update TitleMateri.jsx

--- components/Home/Materi/TitleMateri.jsx
+++ components/Home/Materi/TitleMateri.jsx
@@ -1,12 +1,12 @@
 import AnimatedInObject from "../../GlobalComponents/AnimatedInObject"
 
-const TitleMateri = ({children}) => {
+const TitleMateri = () => {
   const titleAnimation = {
     visible: { opacity: 1, transition: { duration: 0.5 } },
     hidden: { opacity: 0 }
   }
   return (
-    <AnimatedInObject animation={titleAnimation} className="text-white font-poppins lg:text-5xl md:text-3xl text-xl font-bold">{children}</AnimatedInObject>
+    <AnimatedInObject animation={titleAnimation} className="text-white font-poppins lg:text-5xl md:text-3xl text-xl font-bold">Pilih Materi</AnimatedInObject>
   )
 }
 
