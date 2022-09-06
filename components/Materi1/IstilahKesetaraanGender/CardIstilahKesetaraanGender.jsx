4087d15c030b0ecb6cd9467b66deb22c33c86155
rianwp <rianwp2015@gmail.com>
2022-09-06 23:33:16 +0700

    Update CardIstilahKesetaraanGender.jsx

--- components/Materi1/IstilahKesetaraanGender/CardIstilahKesetaraanGender.jsx
+++ components/Materi1/IstilahKesetaraanGender/CardIstilahKesetaraanGender.jsx
@@ -13,10 +13,10 @@ const CardIstilahKesetaraanGender = ({children}) => {
       initial="hidden"
       whileInView="visible"
       whileHover="hover"
-      className="bg-gradient-to-b from-[#0031FF] to-[#B900FF] p-1 rounded-2xl lg:m-8 md:m-4 m-2 relative"
+      className="bg-gradient-to-b from-[#0031FF] to-[#B900FF] p-1 rounded-2xl lg:m-8 md:m-4 m-2 relative sm:w-auto w-full"
     >
       <div className="z-0 absolute top-0 bg-black/90 w-full h-full blur-md"></div>
-      <div className="w-full h-full px-4 py-2">
+      <div className="relative w-full h-full sm:px-6 sm:py-3 px-3 py-2  text-white z-10 font-poppins md:text-base text-xs">
         {children}
       </div>
     </motion.div>
