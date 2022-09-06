ef00d27bbf6ed707e0a064e8fe8f3621f2fad8a7
rianwp <rianwp2015@gmail.com>
2022-09-06 21:00:17 +0700

    Update TitleHero.jsx

--- components/Home/Hero/TitleHero.jsx
+++ components/Home/Hero/TitleHero.jsx
@@ -1,12 +1,12 @@
 import AnimatedInObject from "../../GlobalComponents/AnimatedInObject"
 
-const TitleHero = ({children}) => {
+const TitleHero = () => {
   const titleAnimation = {
-    visible: { opacity: 1, transition: { duration: 0.5 } },
-    hidden: { opacity: 0 }
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+    hidden: { opacity: 0, y: 100 }
   }
   return (
-    <AnimatedInObject animation={titleAnimation} className="text-white font-poppins lg:text-7xl md:text-5xl text-4xl font-bold">{children}</AnimatedInObject>
+    <AnimatedInObject animation={titleAnimation} className="text-white font-poppins lg:text-7xl md:text-5xl text-4xl font-bold neon-shadow">GenEquity</AnimatedInObject>
   )
 }
 
