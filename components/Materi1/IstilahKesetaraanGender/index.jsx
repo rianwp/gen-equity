f8ae7559d048eff58e6fea0f6ba45d4e6eadb6ac
rianwp <rianwp2015@gmail.com>
2022-09-06 23:33:18 +0700

    Update index.jsx

--- components/Materi1/IstilahKesetaraanGender/index.jsx
+++ components/Materi1/IstilahKesetaraanGender/index.jsx
@@ -1,13 +1,14 @@
 import Section from "../../GlobalComponents/Section"
+import CardContainer from "./CardContainer"
 
 const IstilahKesetaraanGender = () => {
   return (
     <Section>
       <div className="w-full h-full flex items-center justify-center flex-col text-center">
-        
+        <CardContainer/>
       </div>
     </Section>
   )
 }
 
-export default PengertianKesetaraanGender
+export default IstilahKesetaraanGender
