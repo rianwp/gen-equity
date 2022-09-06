16a6304f7899d1bfaf92262e59553d9716dffd4c
rianwp <rianwp2015@gmail.com>
2022-09-06 21:00:42 +0700

    Create index.jsx

---
+++ components/Materi/Intermezzo1/index.jsx
@@ -0,0 +1,16 @@
+import Section from '../../GlobalComponents/Section'
+import CardIntermezzo1 from './CardIntermezzo1'
+import TextIntermezzo1 from './TextIntermezzo1'
+
+const Intermezzo1 = () => {
+  return (
+    <Section>
+      <div className="w-full h-full flex items-center justify-center flex-col text-center">
+        <CardIntermezzo1/>
+        <TextIntermezzo1/>
+      </div>
+    </Section>
+  )
+}
+
+export default Intermezzo1
