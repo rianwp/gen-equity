50c62faa43ac5fcc6af9bb11a8d7d7de39a7abe6
rianwp <rianwp2015@gmail.com>
2022-09-06 22:28:42 +0700

    Create CardContainer.jsx

---
+++ components/Materi1/IstilahKesetaraanGender/CardContainer.jsx
@@ -0,0 +1,28 @@
+import CardIstilahKesetaraanGender from "./CardIstilahKesetaraanGender"
+
+const CardContainer = () => {
+  const dataCard = [
+    {
+      title: "Yuk, pahami tentang kesetaraan gender",
+      link: "/kesetaraangender",
+      img: "../img/womenman.svg"
+    },
+    {
+      title: "Kenali lika-liku menjadi perempuan",
+      link: "",
+      img: "../img/womenmatericard.svg"
+    },
+    {
+      title: "Stereotip laki-laki dalam masyarakat",
+      link: "",
+      img: "../img/manmatericard.svg"
+    }
+  ]
+  return (
+    <div className="flex flex-row justify-between">
+          
+    </div>
+  )
+}
+
+export default CardContainer
