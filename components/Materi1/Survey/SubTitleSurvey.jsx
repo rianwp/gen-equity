6c8ee7b8074b2d6166cd25ee703f7dbb36b0d93f
rianwp <rianwp2015@gmail.com>
2022-09-06 22:29:01 +0700

    Create SubTitleSurvey.jsx

---
+++ components/Materi1/Survey/SubTitleSurvey.jsx
@@ -0,0 +1,13 @@
+import AnimatedInObject from "../../GlobalComponents/AnimatedInObject"
+
+const SubTitleHero = ({children}) => {
+  const subTitleAnimation ={
+    visible: { opacity: 1,  transition: { duration: 0.5, delay: 0.3} },
+    hidden: { opacity: 0}
+  }
+  return (
+    <AnimatedInObject animation={subTitleAnimation} className="text-white font-poppins lg:text-2xl md:text-xl text-sm mt-4 font-light">{children}</AnimatedInObject>
+  )
+}
+
+export default SubTitleHero
