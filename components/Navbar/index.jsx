c2e71d1fedadf4adc71006679a5178f826bc7395
rianwp <rianwp2015@gmail.com>
2022-09-06 21:43:25 +0700

    Update index.jsx

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -5,7 +5,7 @@ import Navitems from "./Navitems"
 const Navbar = () => {
   return (
     <div className="w-full bg-white/10 fixed top-0 z-50 backdrop-blur-md border-b border-white/30 rounded-b-md">
-      <Container className="flex flex-row justify-between items-center py-4">
+      <Container className="flex flex-row justify-between items-center p-4">
         <Logo/>
         <Navitems/>
       </Container>
