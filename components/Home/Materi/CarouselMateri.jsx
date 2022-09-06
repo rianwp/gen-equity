eb7285484735136dd0cd3c3e72872e4c4c6752ef
rianwp <rianwp2015@gmail.com>
2022-09-06 21:00:23 +0700

    Update CarouselMateri.jsx

--- components/Home/Materi/CarouselMateri.jsx
+++ components/Home/Materi/CarouselMateri.jsx
@@ -2,12 +2,13 @@ import { motion, useAnimation } from "framer-motion"
 import CardButton from "./CardButton"
 import CardMateri from "./CardMateri"
 import { useEffect, useState, useRef } from "react"
+import CarouselController from "../../GlobalComponents/CarouselController"
 
 const CarouselMateri = () => {
   const dataCard = [
     {
       title: "Yuk, pahami tentang kesetaraan gender",
-      link: "",
+      link: "/kesetaraangender",
       img: "../img/womenman.svg"
     },
     {
@@ -38,21 +39,21 @@ const CarouselMateri = () => {
       x: info.offset.x
     })
   }
-  let slideLength = 0
+  const slideLength = useRef(0)
 
   const handlePanEnd = (event, info) => {
     const x = info.offset.x
-    if(x < -100 && slideLength < 200){
-      slideLength += 100
+    if(x < -50 && slideLength.current < 200){
+      slideLength.current += 100
       controls.start({
         x: 0,
-        translateX: "-" + slideLength + "%" 
+        translateX: "-" + slideLength.current + "%" 
       })
-    } else if(x > 100 && slideLength > 0){
-      slideLength -= 100
+    } else if(x > 50 && slideLength.current > 0){
+      slideLength.current -= 100
       controls.start({
         x: 0,
-        translateX: "-" + slideLength + "%" 
+        translateX: "-" + slideLength.current + "%" 
       })
     } else{
       controls.start({
@@ -60,40 +61,76 @@ const CarouselMateri = () => {
       })
     }
   }
-  useEffect(() => {
-    if(slideLength === 0){
+  const clickSlideRight = () => {
+    if(slideLength.current < 200){
+      slideLength.current += 100
       controls.start({
-        translateX: 0 + "%" 
+        x: 0,
+        translateX: "-" + slideLength.current + "%" 
+      })
+    } else{
+      controls.start({
+        x: [0, -25, 0],
+        transition: {duration: 0.3}
       })
     }
+  }
+  const clickSlideLeft = () => {
+    if(slideLength.current > 0){
+      slideLength.current -= 100
+      controls.start({
+        x: 0,
+        translateX: "-" + slideLength.current + "%" 
+      })
+    } else{
+      controls.start({
+        x: [0, 25, 0],
+        transition: {duration: 0.3}
+      })
+    }
+  }
+  useEffect(() => {
+    if(windowWidth > 500){
+      slideLength.current = 0
+    }
   },[windowWidth])
+
   if(windowWidth <= 500){
     return(
-      <motion.div
-        className="flex flex-row items-center justify-start cursor-pointer"
-        animate={controls}
-        
-      >
-        {dataCard.map((card, index) => 
-          <motion.div key={index}
-          onPan={handlePan}
-        onPanEnd={handlePanEnd} className="w-full shrink-0 flex flex-row justify-center">
-            <CardMateri>
-              <motion.img src={card.img} className="absolute z-10 p-4 top-0 w-full h-full"/>
-              <div className="z-20 absolute top-0 flex flex-col justify-center text-center items-center w-full h-full px-2">
-                <p className="text-white font-poppins lg:text-2xl md:text-xl text-sm mt-4 font-light">
-                  {card.title}
-                </p>
-              </div>
-              <div className="z-20 absolute top-0 flex flex-row justify-center items-end w-full h-full">
-                <div className="h-1/4">
-                  <CardButton link={card.link}/>
+      <div className="relative w-full">
+        <div className="absolute h-full w-full flex flex-row justify-between items-center">          
+          <CarouselController onClick={clickSlideLeft} variant="left" bgColor="bg-blue-900"/>
+          <CarouselController onClick={clickSlideRight} variant="right" bgColor="bg-blue-900"/>
+        </div>
+        <motion.div
+          className="flex flex-row items-center justify-start"
+          whileTap={{cursor: "grabbing"}}
+          whileHover={{cursor: "grab"}}
+          animate={controls}
+        >
+          {dataCard.map((card, index) => 
+            <motion.div key={index}
+              onPan={handlePan}
+              onPanEnd={handlePanEnd} 
+              className="relative w-full shrink-0 flex flex-row justify-center"
+            >
+              <CardMateri>
+                <motion.img src={card.img} className="absolute z-10 p-4 top-0 w-full h-full"/>
+                <div className="absolute top-0 flex flex-col justify-center text-center items-center w-full h-full px-2">
+                  <p className="text-white font-poppins lg:text-2xl md:text-xl text-sm mt-4 font-light">
+                    {card.title}
+                  </p>
                 </div>
-              </div>
-            </CardMateri>
-          </motion.div>
-        )}
-      </motion.div>
+                <div className="z-20 absolute top-0 flex flex-row justify-center items-end w-full h-full">
+                  <div className="h-1/4">
+                    <CardButton link={card.link}/>
+                  </div>
+                </div>
+              </CardMateri>
+            </motion.div>
+          )}
+        </motion.div>
+      </div>
     )
   }
   return (
