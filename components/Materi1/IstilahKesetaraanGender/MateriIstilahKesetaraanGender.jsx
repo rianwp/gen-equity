ce3a9eb21aa6fc8585d450d13fb803f40a71b96d
rianwp <rianwp2015@gmail.com>
2022-09-06 23:33:21 +0700

    Create MateriIstilahKesetaraanGender.jsx

---
+++ components/Materi1/IstilahKesetaraanGender/MateriIstilahKesetaraanGender.jsx
@@ -0,0 +1,11 @@
+import { motion } from "framer-motion"
+
+const MateriIstilahKesetaraanGender = ({children}) => {
+  return (
+    <motion.div className="w-full font-poppins sm:text-base text-xs text-white">
+      {children}
+    </motion.div>
+  )
+}
+
+export default MateriIstilahKesetaraanGender
