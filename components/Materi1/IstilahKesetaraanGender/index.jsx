89983db72fc663153a6969c121eb166c9d49df29
rianwp <rianwp2015@gmail.com>
2022-09-06 22:28:48 +0700

    Create index.jsx

---
+++ components/Materi1/IstilahKesetaraanGender/index.jsx
@@ -0,0 +1,13 @@
+import Section from "../../GlobalComponents/Section"
+
+const IstilahKesetaraanGender = () => {
+  return (
+    <Section>
+      <div className="w-full h-full flex items-center justify-center flex-col text-center">
+        
+      </div>
+    </Section>
+  )
+}
+
+export default PengertianKesetaraanGender
