8e359d3d74d51913085d6a8ddaeb869cf4b68b22
rianwp <rianwp2015@gmail.com>
2022-09-06 21:43:02 +0700

    Create index.jsx

---
+++ components/Materi/PengertianKesetaraanGender/index.jsx
@@ -0,0 +1,14 @@
+import Section from "../../GlobalComponents/Section"
+import CardPengertianKesetaraanGender from "./CardPengertianKesetaraanGender"
+
+const PengertianKesetaraanGender = () => {
+  return (
+    <Section>
+      <div className="w-full h-full flex items-center justify-center flex-col text-center">
+        <CardPengertianKesetaraanGender/>
+      </div>
+    </Section>
+  )
+}
+
+export default PengertianKesetaraanGender
