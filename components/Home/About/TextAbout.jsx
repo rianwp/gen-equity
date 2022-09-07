06047ee9debefb747e476dc38169ff3fae5691c9
rianwp <rianwp2015@gmail.com>
2022-09-07 19:40:36 +0700

    Update TextAbout.jsx

--- components/Home/About/TextAbout.jsx
+++ components/Home/About/TextAbout.jsx
@@ -8,7 +8,7 @@ const TextAbout = () => {
   }
   return (
     <Container className="lg:w-1/2 w-full lg:mt-8 mt-4">
-      <AnimatedInObject animation={aboutAnimation} className="text-white font-poppins lg:text-2xl md:text-xl text-sm">Platform edukasi GenEquity dibuat dengan tujuan mengedukasi para generasi muda agar mengetahui tentang pentingnya persamaan gender. Dikarenakan hal tersebut  masih banyak terjadi diskriminasi berdasarkan gender yang  terjadi pada aspek kehidupan. Kami mempunyai harapan dengan adanya GenEquity ini, dapat membuka wawasan masyarakat Indonesia agar lebih menaruh perhatiannya terhadap kesetaraan gender.</AnimatedInObject>
+      <AnimatedInObject animation={aboutAnimation} className="text-white font-poppins md:text-xl text-sm">Platform edukasi GenEquity dibuat dengan tujuan mengedukasi para generasi muda agar mengetahui tentang pentingnya persamaan gender. Dikarenakan hal tersebut  masih banyak terjadi diskriminasi berdasarkan gender yang  terjadi pada aspek kehidupan. Kami mempunyai harapan dengan adanya GenEquity ini, dapat membuka wawasan masyarakat Indonesia agar lebih menaruh perhatiannya terhadap kesetaraan gender.</AnimatedInObject>
     </Container>
   )
 }
