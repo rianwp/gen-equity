7f2f91bf2d259425712dd8409f35f3b32c7fe91e
rianwp <rianwp2015@gmail.com>
2022-09-07 19:40:56 +0700

    Update TextPerkenalan.jsx

--- components/Home/Perkenalan/TextPerkenalan.jsx
+++ components/Home/Perkenalan/TextPerkenalan.jsx
@@ -8,7 +8,7 @@ const TextPerkenalan = () => {
   }
   return (
     <Container className="lg:w-1/2 w-full lg:mt-8 mt-4">
-      <AnimatedInObject animation={perkenalanAnimation} className="text-white font-poppins lg:text-2xl md:text-xl text-sm">Halo sobat GE, selamat datang di web GenEquity. Tenang jangan takut, aku tidak menyengat kamu kok, hehe. Kenalin dulu namaku Lee, si lebah cerdas yang akan menemani kamu dalam menjelaskan banyak materi dan survey tentang Gender Equality nih. Yuk ikutin perjalananku untuk lebih tau tentang Gender Equality 😁.</AnimatedInObject>
+      <AnimatedInObject animation={perkenalanAnimation} className="text-white font-poppins md:text-xl text-sm">Halo sobat GE, selamat datang di web GenEquity. Tenang jangan takut, aku tidak menyengat kamu kok, hehe. Kenalin dulu namaku Lee, si lebah cerdas yang akan menemani kamu dalam menjelaskan banyak materi dan survey tentang Gender Equality nih. Yuk ikutin perjalananku untuk lebih tau tentang Gender Equality 😁.</AnimatedInObject>
     </Container>
   )
 }
