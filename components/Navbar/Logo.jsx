2e885e07c002ceef4c0fb07128393d90dd837000
rianwp <rianwp2015@gmail.com>
2022-09-08 00:28:46 +0700

    Update Logo.jsx

--- components/Navbar/Logo.jsx
+++ components/Navbar/Logo.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 const Logo = () => {
   return (
     <Link href="/">
-      <a><img className="sm:h-10 h-5" src="../img/logo.png" alt="" /></a>
+      <a><img className="sm:h-10 h-8" src="../img/logo.png" alt="" /></a>
     </Link>
     
   )
