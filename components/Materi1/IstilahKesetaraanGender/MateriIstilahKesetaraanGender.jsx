4026655c6ad429c3cdc23ad8dac03d48d611f449
rianwp <rianwp2015@gmail.com>
2022-09-07 19:41:18 +0700

    Update MateriIstilahKesetaraanGender.jsx

--- components/Materi1/IstilahKesetaraanGender/MateriIstilahKesetaraanGender.jsx
+++ components/Materi1/IstilahKesetaraanGender/MateriIstilahKesetaraanGender.jsx
@@ -1,8 +1,18 @@
 import { motion } from "framer-motion"
 
-const MateriIstilahKesetaraanGender = ({children}) => {
+const MateriIstilahKesetaraanGender = ({children, key}) => {
+  const materiAnimation ={
+    visible: { opacity: 1, transition: { duration: 0.3} },
+    hidden: { opacity: 0 },
+  }
   return (
-    <motion.div className="w-full font-poppins sm:text-base text-xs text-white">
+    <motion.div
+      variants={materiAnimation}
+      key={key}
+      animate="visible"
+      initial="hidden"
+      className="sm:w-3/4 w-full font-poppins sm:text-base text-xs text-white mx-auto text-justify"
+    >
       {children}
     </motion.div>
   )
