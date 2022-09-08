22b48ead5cb4c10d42ad83013f3eb4cb4a833623
rianwp <rianwp2015@gmail.com>
2022-09-08 15:59:54 +0700

    Create TitleMateri.jsx

---
+++ components/Page1/Materi/TitleMateri.jsx
@@ -0,0 +1,13 @@
+import AnimatedInObject from "../../GlobalComponents/AnimatedInObject"
+
+const TitleMateri = () => {
+  const titleAnimation = {
+    visible: { opacity: 1, transition: { duration: 0.5 } },
+    hidden: { opacity: 0 }
+  }
+  return (
+    <AnimatedInObject animation={titleAnimation} className="text-white font-poppins md:text-4xl text-xl font-bold">Pilih Materi</AnimatedInObject>
+  )
+}
+
+export default TitleMateri
