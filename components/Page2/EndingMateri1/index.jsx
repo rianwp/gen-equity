f9146bb6a16f0ef15b6081fd15bfc9b8d6027045
rianwp <rianwp2015@gmail.com>
2022-09-08 16:00:01 +0700

    Create index.jsx

---
+++ components/Page2/EndingMateri1/index.jsx
@@ -0,0 +1,16 @@
+import Section from "../../GlobalComponents/Section"
+import LogoEndingMateri1 from "./LogoEndingMateri1"
+import TextEndingMateri1 from "./TextEndingMateri1"
+
+const EndingMateri1 = () => {
+  return (
+    <Section>
+      <div className="w-full h-full flex items-center justify-center flex-col text-center">
+        <LogoEndingMateri1/>
+        <TextEndingMateri1/>
+      </div>
+    </Section>
+  )
+}
+
+export default EndingMateri1
