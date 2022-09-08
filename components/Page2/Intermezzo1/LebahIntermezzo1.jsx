c39ee279cc395656a9b53b07cac86fe5c4121bcf
rianwp <rianwp2015@gmail.com>
2022-09-08 16:00:09 +0700

    Create LebahIntermezzo1.jsx

---
+++ components/Page2/Intermezzo1/LebahIntermezzo1.jsx
@@ -0,0 +1,16 @@
+import AnimatedInObject from "../../GlobalComponents/AnimatedInObject"
+
+const LebahIntermezzo1 = () => {
+  const lebahAnimation = {
+    visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+    hidden: { opacity: 0, x: 100 }
+  }
+  return (
+    <AnimatedInObject className="absolute z-0 -bottom-1 -right-16 w-[200px] sm:block hidden" animation={lebahAnimation}>
+      <img className="object-cover object-bottom w-full" src="../img/lebahintermezzo1.svg"/>
+    </AnimatedInObject>
+  )
+}
+
+
+export default LebahIntermezzo1
