ec7dc1d4a71a15e85944008b840e417e4bb1e318
rianwp <rianwp2015@gmail.com>
2022-09-08 16:00:20 +0700

    Create MateriIstilahKesetaraanGender.jsx

---
+++ components/Page2/IstilahKesetaraanGender/MateriIstilahKesetaraanGender.jsx
@@ -0,0 +1,21 @@
+import { motion } from "framer-motion"
+
+const MateriIstilahKesetaraanGender = ({children, key}) => {
+  const materiAnimation ={
+    visible: { opacity: 1, transition: { duration: 0.3} },
+    hidden: { opacity: 0 },
+  }
+  return (
+    <motion.div
+      variants={materiAnimation}
+      key={key}
+      animate="visible"
+      initial="hidden"
+      className="sm:w-3/4 w-full font-poppins md:text-xl text-sm text-white mx-auto text-justify"
+    >
+      {children}
+    </motion.div>
+  )
+}
+
+export default MateriIstilahKesetaraanGender
