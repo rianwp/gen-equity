5142ffa7f159a43fd81b0d9afbc3210f3ee88dc7
rianwp <rianwp2015@gmail.com>
2022-09-10 03:12:59 +0700

    Update CardIntermezzo1.jsx

--- components/Page2/Intermezzo1/CardIntermezzo1.jsx
+++ components/Page2/Intermezzo1/CardIntermezzo1.jsx
@@ -7,9 +7,9 @@ const CardIntermezzo1 = () => {
     hidden: { opacity: 0}
   }
   return (
-    <AnimatedInObject animation={cardAnimation} className="relative lg:w-1/2 md:w-3/4 w-full bg-[#540062] rounded-xl p-4 sm:min-h-[300px] h-[250px] card-shadow">
-      <div className="relative text-justify z-10 text-white indent-16 font-poppins md:text-base sm:text-sm text-xs">Hallo sobat GE, ketemu lagi nih sama Lee.  Apakah kalian sudah membaca survey diatas? Nah, kalau sudah. Berdasarkan data tersebut,  Lee masih menemukan ada yang kurang paham nih perihal kesetaraan gender. Baiklah, ikuti perjalananku yuk. Untuk membahas lebih dalam tentang kesetaraan gender.</div>
-      <img className="absolute bottom-0 left-0 object-cover object-bottom" src="../img/bungaintermezzo1.svg"/>
+    <AnimatedInObject animation={cardAnimation} className="relative lg:w-1/2 md:w-3/4 w-full bg-[#540062] rounded-xl lg:p-8 p-4 sm:min-h-[300px] h-[250px] card-shadow">
+      <div className="relative text-justify z-10 text-white md:indent-16 indent-8 font-poppins md:text-base sm:text-sm text-xs">Hallo sobat GE, ketemu lagi nih sama Lee.  Apakah kalian sudah membaca survey diatas? Nah, kalau sudah. Berdasarkan data tersebut,  Lee masih menemukan ada yang kurang paham nih perihal kesetaraan gender. Baiklah, ikuti perjalananku yuk. Untuk membahas lebih dalam tentang kesetaraan gender.</div>
+      <img className="absolute bottom-0 left-0 object-cover object-bottom" src="../img/bungakiri.svg"/>
       <LebahIntermezzo1/>
     </AnimatedInObject>
   )
