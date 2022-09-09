0698933eeee7f6e837a744527bb5e97bb10d0777
rianwp <rianwp2015@gmail.com>
2022-09-10 03:12:14 +0700

    Update CardButton.jsx

--- components/GlobalComponents/CardButton.jsx
+++ components/GlobalComponents/CardButton.jsx
@@ -1,16 +1,24 @@
 import Link from 'next/link'
 
-const CardButton = ({link}) => {
-  return (
-    <Link href={link}>
-      <a
-        className="text-white font-poppins font-light lg:text-lg md:text-base text-sm mt-4 bg-gradient-to-b from-[#001d92] to-[#700099] rounded-3xl md:px-4 md:py-2 px-3 py-1"
-      >
+const CardButton = ({link, onClick}) => {
+  if(link !== undefined){
+    return (
+      <Link href={link}>
+        <a
+          className="text-white font-poppins font-light lg:text-lg md:text-base text-sm mt-4 bg-gradient-to-b from-[#001d92] to-[#700099] rounded-3xl md:px-4 md:py-2 px-3 py-1"
+        >
+          Mulai Baca
+        </a>
+      </Link>
+    )
+  } else{
+    return (
+      <a role="button" onClick={onClick} className="text-white font-poppins font-light lg:text-lg md:text-base text-sm mt-4 bg-gradient-to-b from-[#001d92] to-[#700099] rounded-3xl md:px-4 md:py-2 px-3 py-1">
         Mulai Baca
       </a>
-    </Link>
-    
-  )
+    )
+  }
+  
 }
 
 export default CardButton
