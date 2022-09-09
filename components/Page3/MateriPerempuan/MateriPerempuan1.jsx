139509306bcaf4f8a65020fa8faa778d80bb0a9c
rianwp <rianwp2015@gmail.com>
2022-09-10 03:13:21 +0700

    Create MateriPerempuan1.jsx

---
+++ components/Page3/MateriPerempuan/MateriPerempuan1.jsx
@@ -0,0 +1,15 @@
+const MateriPerempuan1 = () => {
+  return (
+    <>
+      <div className="mb-8 text-white font-poppins md:text-xl text-sm font-bold">Perempuan Harus Bisa Memasak</div>
+      <div className="text-justify text-white md:indent-16 indent-8 font-poppins md:text-base sm:text-sm text-xs">
+        Sobat GE menurut kamu haruskah perempuan bisa memasak ? Kalau menurut Lee sih ya, perempuan tidak diharuskan untuk bisa memasak kok, karena walaupun tidak bisa memasak pun, dia masih mempunyai hal lain yang merupakan kelebihan dari sosok perempuan. Karena sejatinya, setiap orang memiliki kelebihannya masing-masing. 
+        <br/>
+        <br/>
+        Memasak itu bukan sebuah hal wajib yang harus dikuasai oleh seorang perempuan. Jadi kamu jangan merasa insecure  ya, walaupun kamu gak bisa masak tetap hargai dirimu ya sobat GE. Sekarang banyak juga kok pasangan yang tidak menuntut sang istri untuk harus bisa memasak karena mereka tau bahwa sterotipe tersebut kurang baik untuk perempuan. Bagaimana pendapatmu apakah kamu juga sependapat ?
+      </div>
+    </>
+  )
+}
+
+export default MateriPerempuan1
