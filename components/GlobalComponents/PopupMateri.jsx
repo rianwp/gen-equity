021a4ff7a692ccc3891cdb22d6a6d5c72d0e2e09
rianwp <rianwp2015@gmail.com>
2022-09-10 03:12:35 +0700

    Create PopupMateri.jsx

---
+++ components/GlobalComponents/PopupMateri.jsx
@@ -0,0 +1,57 @@
+import { motion, AnimatePresence } from "framer-motion"
+import { useEffect, useState } from "react"
+
+const PopupMateri = ({ children, show }) => {
+  const [isShow, setIsShow] = useState(false)
+  useEffect(() => {
+    setIsShow(show)
+  }, [show])
+  useEffect(() => {
+    if(isShow){
+      document.body.style.overflow = 'hidden'
+    } else{
+      document.body.style.overflow = 'visible'
+    }
+  }, [isShow])
+  const bgAnimation = {
+    initial: { opacity: 0 },
+    in: { opacity: 1,  transition: { duration: 0.3 } },
+    out: { opacity: 0,  transition: { duration: 0.3 } }
+  }
+  const cardAnimation = {
+    initial: { opacity: 0, y: -100 },
+    in: { opacity: 1, y: 0,  transition: { duration: 0.3, delay: 0.3} },
+    out: { opacity: 0, y: 100, transition: { duration: 0.3 } }
+  }
+  return (
+    <AnimatePresence>
+      {isShow && (
+        <motion.div
+          key="popup"
+          variants={bgAnimation}
+          initial="initial"
+          animate="in"
+          exit="out"
+          className="fixed top-0 left-0 z-50 w-full h-screen bg-black/50 flex flex-row items-center justify-center"
+        >
+          <motion.div 
+            variants={cardAnimation}
+            initial="initial"
+            animate="in"
+            exit="out"
+            className="relative lg:w-1/2 md:w-3/4 w-full bg-[#540062] rounded-xl sm:p-8 p-4 min-h-[500px] max-h-[calc(100%-2rem)] overflow-auto m-4"
+          >
+            <div className="relative z-10 w-full">
+              {children}
+            </div>
+            <img className="absolute bottom-0 left-0 object-cover object-bottom" src="../img/bungakiri.svg"/>
+            <img className="absolute bottom-0 right-0 object-cover object-bottom" src="../img/bungakanan.svg"/>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  )
+} 
+
+
+export default PopupMateri
