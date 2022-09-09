cb38233f20db2a68c112f7686b8bad5de4fb817b
rianwp <rianwp2015@gmail.com>
2022-09-10 03:12:52 +0700

    Update index.jsx

--- components/Page2/EndingMateri1/index.jsx
+++ components/Page2/EndingMateri1/index.jsx
@@ -1,12 +1,12 @@
 import Section from "../../GlobalComponents/Section"
-import LogoEndingMateri1 from "./LogoEndingMateri1"
+import LogoEnding from "../../GlobalComponents/LogoEnding"
 import TextEndingMateri1 from "./TextEndingMateri1"
 
 const EndingMateri1 = () => {
   return (
     <Section>
       <div className="w-full h-full flex items-center justify-center flex-col text-center">
-        <LogoEndingMateri1/>
+        <LogoEnding/>
         <TextEndingMateri1/>
       </div>
     </Section>
