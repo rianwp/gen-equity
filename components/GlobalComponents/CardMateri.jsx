41b2646a7a27c23b477765618e3b0f11762810ce
rianwp <rianwp2015@gmail.com>
2022-09-10 03:12:17 +0700

    Update CardMateri.jsx

--- components/GlobalComponents/CardMateri.jsx
+++ components/GlobalComponents/CardMateri.jsx
@@ -4,11 +4,9 @@ const CardMateri = ({children, variant}) => {
   const addVariant = {
     rectangle: {
       size: "lg:w-[300px] lg:h-[400px] md:w-[250px] md:h-[300px] w-[150px] h-[200px]",
-      opacity: ""
     },
     square: {
-      size: "lg:w-[300px] lg:h-[400px] md:w-[250px] md:h-[300px] w-[150px] h-[200px]",
-      opacity: "bg-opacity-50"
+      size: "lg:w-[400px] lg:h-[400px] md:w-[300px] md:h-[300px] w-[200px] h-[200px]",
     },
   }
   const pickedVariant = addVariant[variant]
@@ -25,9 +23,9 @@ const CardMateri = ({children, variant}) => {
       initial="hidden"
       whileInView="visible"
       whileHover="hover"
-      className={`shrink-0 ${pickedVariant.size} bg-gradient-to-b from-[#0031FF] to-[#B900FF] p-1 rounded-2xl lg:m-8 md:m-4 m-2 relative ${pickedVariant.opacity}`}
+      className={`shrink-0 ${pickedVariant.size} bg-gradient-to-b from-[#0031FF] to-[#B900FF] p-1 rounded-2xl lg:m-8 md:m-4 m-2 relative`}
     >
-      <div className="absolute top-0 bg-black/90 w-full h-full blur-md"></div>
+      <div className={`absolute top-0 bg-black/90 w-full h-full blur-md`}></div>
       <div className="w-full h-full">
         {children}
       </div>
