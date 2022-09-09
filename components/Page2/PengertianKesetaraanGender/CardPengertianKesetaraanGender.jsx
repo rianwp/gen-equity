0226864cfc36b21f565e0b4105314077b4cf31a7
rianwp <rianwp2015@gmail.com>
2022-09-10 03:13:05 +0700

    Update CardPengertianKesetaraanGender.jsx

--- components/Page2/PengertianKesetaraanGender/CardPengertianKesetaraanGender.jsx
+++ components/Page2/PengertianKesetaraanGender/CardPengertianKesetaraanGender.jsx
@@ -8,9 +8,9 @@ const CardPengertianKesetaraanGender = () => {
   }
   return (
     <AnimatedInObject animation={cardAnimation} className="lg:w-1/2 md:w-3/4 w-full">
-      <div className="text-justify z-10 mb-2 text-white font-poppins md:text-xl text-sm font-bold">Lalu apa itu kesetaraan gender Lee?</div>
-      <div className="relative bg-[#540062] rounded-xl p-4 lg:min-h-[500px] min-h-[400px] card-shadow">
-        <div className="relative text-justify z-10 text-white indent-16 font-poppins md:text-base sm:text-sm text-xs">Baiklah Lee akan membantu sobat GE, atas pertanyaan tentang kesetaraan gender. Menurut WHO pengertian kesetaraan gender adalah kesamaan kondisi bagi laki-laki atau perempuan guna memperoleh kesempatan serta hak-haknya sebagai manusia, agar mampu berpartisipasi dalam kegiatan politik, sosial budaya, pendidikan, serta kesamaan dalam menikmati hasil pembangunan. Nah, didalam gender equailty ini ada beberapa istilah yang sering digunakan  sobat Lee. Apa saja yuk cari tau.</div>
+      <div className="text-justify z-10 mb-2 text-white font-poppins md:text-xl text-sm font-bold neon-shadow">Lalu apa itu kesetaraan gender Lee?</div>
+      <div className="relative bg-[#540062] rounded-xl lg:p-8 p-4 lg:min-h-[500px] min-h-[400px] card-shadow">
+        <div className="relative text-justify z-10 text-white md:indent-16 indent-8 font-poppins md:text-base sm:text-sm text-xs">Baiklah Lee akan membantu sobat GE, atas pertanyaan tentang kesetaraan gender. Menurut WHO pengertian kesetaraan gender adalah kesamaan kondisi bagi laki-laki atau perempuan guna memperoleh kesempatan serta hak-haknya sebagai manusia, agar mampu berpartisipasi dalam kegiatan politik, sosial budaya, pendidikan, serta kesamaan dalam menikmati hasil pembangunan. Nah, didalam gender equailty ini ada beberapa istilah yang sering digunakan  sobat Lee. Apa saja yuk cari tau.</div>
         <LebahPengertianKesetaraanGender/>
       </div>
     </AnimatedInObject>
