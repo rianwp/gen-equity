600763be6344a0cea2246c3283006b4cdce0a570
rianwp <rianwp2015@gmail.com>
2022-09-10 03:13:37 +0700

    Create lakilaki.js

---
+++ pages/lakilaki.js
@@ -0,0 +1,20 @@
+import Head from "next/head"
+import Container from "../components/GlobalComponents/Container"
+
+const Page4 = () => {
+  return (
+    <>
+      <Head>
+        <title>Stereotipe Laki-Laki</title>
+      </Head>
+      <div className="bg-materi3 bg-cover bg-top bg-no-repeat overflow-hidden">
+        <Container className="mt-4 sm:mt-0">
+          <div className="px-4">
+          </div>
+        </Container>
+      </div>
+    </>
+  )
+}
+
+export default Page4
