643dc9ea30edea618dfe2ddabf717819a68079aa
rianwp <rianwp2015@gmail.com>
2022-09-10 03:12:40 +0700

    Update index.jsx

--- components/Page1/About/index.jsx
+++ components/Page1/About/index.jsx
@@ -6,12 +6,8 @@ const About = () => {
   return (
     <Section>
       <div id="tentang" className="w-full h-full flex items-center justify-center flex-col text-center">
-        <div className="w-full md:py-8 py-4">
-          <TitleAbout>Gen Equity</TitleAbout>
-        </div>
-        <div className="w-full md:py-8 py-4">
-          <TextAbout/>
-        </div>
+        <TitleAbout>Gen Equity</TitleAbout>
+        <TextAbout/>
       </div>
     </Section>
   )
