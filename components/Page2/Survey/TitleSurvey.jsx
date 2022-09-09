d07352327f33380488291aee563cc743a6693931
rianwp <rianwp2015@gmail.com>
2022-09-10 03:13:07 +0700

    Update TitleSurvey.jsx

--- components/Page2/Survey/TitleSurvey.jsx
+++ components/Page2/Survey/TitleSurvey.jsx
@@ -6,7 +6,7 @@ const TitleSurvey = () => {
     hidden: { opacity: 0 }
   }
   return (
-    <AnimatedInObject animation={titleAnimation} className="text-white font-poppins md:text-4xl text-xl font-bold">Yuk, Pahami tentang Kesetaraan Gender</AnimatedInObject>
+    <AnimatedInObject animation={titleAnimation} className="text-white font-poppins md:text-4xl text-xl font-bold neon-shadow">Yuk, Pahami tentang Kesetaraan Gender</AnimatedInObject>
   )
 }
 
