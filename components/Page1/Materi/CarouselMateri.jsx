7a2b51c785190f60e48712f1724c0ea473583d35
rianwp <rianwp2015@gmail.com>
2022-09-10 23:43:59 +0700

    Update CarouselMateri.jsx

--- components/Page1/Materi/CarouselMateri.jsx
+++ components/Page1/Materi/CarouselMateri.jsx
@@ -1,7 +1,7 @@
 import { motion, useAnimation } from "framer-motion"
 import CardButton from "../../GlobalComponents/CardButton"
 import CardMateri from "../../GlobalComponents/CardMateri"
-import { useEffect, useState, useRef } from "react"
+import { useRef } from "react"
 import CarouselController from "../../GlobalComponents/CarouselController"
 
 const CarouselMateri = () => {
@@ -22,16 +22,6 @@ const CarouselMateri = () => {
       img: "../img/manmatericard.svg"
     }
   ]
-  const [windowWidth, setWindowWidth] = useState(0)
-  useEffect(()=>{
-    if(typeof window !== "undefined"){
-      setWindowWidth(window.innerWidth)
-      window.addEventListener('resize', () => {
-        setWindowWidth(window.innerWidth)
-      })
-    }
-  },[])
-
   const controls = useAnimation()
 
   const handleDrag = (event, info) => {
@@ -89,15 +79,9 @@ const CarouselMateri = () => {
       })
     }
   }
-  useEffect(() => {
-    if(windowWidth > 500){
-      slideLength.current = 0
-    }
-  },[windowWidth])
-
-  if(windowWidth <= 500){
-    return(
-      <div className="relative w-full">
+  return (
+    <>
+      <div className="relative w-full sm:hidden block">
         <div className="absolute h-full w-full flex flex-row justify-between items-center">          
           <CarouselController onClick={clickSlideLeft} variant="left" bgColor="bg-blue-900"/>
           <CarouselController onClick={clickSlideRight} variant="right" bgColor="bg-blue-900"/>
@@ -109,7 +93,9 @@ const CarouselMateri = () => {
           animate={controls}
         >
           {dataCard.map((card, index) => 
-            <motion.div key={index}
+            <motion.div
+              
+              key={index}
               drag="x"
               dragConstraints={{left: 0, right: 0}}
               onDrag={handleDrag}
@@ -135,26 +121,24 @@ const CarouselMateri = () => {
           )}
         </motion.div>
       </div>
-    )
-  }
-  return (
-    <div className="flex flex-row items-center justify-center">
-      {dataCard.map((card, index) => 
-        <CardMateri key={index} variant="rectangle">
-          <img src={card.img} className="absolute z-10 p-4 top-0 w-full h-full"/>
-          <div className="z-20 absolute top-0 flex flex-col justify-center text-center items-center w-full h-full px-2">
-            <p className="text-white font-poppins lg:text-2xl md:text-xl text-sm mt-4 font-light">
-              {card.title}
-            </p>
-          </div>
-          <div className="z-20 absolute top-0 flex flex-row justify-center items-end w-full h-full">
-            <div className="h-1/4">
-              <CardButton link={card.link}/>
+      <div className="flex-row items-center justify-center sm:flex hidden">
+        {dataCard.map((card, index) => 
+          <CardMateri key={index} variant="rectangle">
+            <img src={card.img} className="absolute z-10 p-4 top-0 w-full h-full"/>
+            <div className="z-20 absolute top-0 flex flex-col justify-center text-center items-center w-full h-full px-2">
+              <p className="text-white font-poppins lg:text-2xl md:text-xl text-sm mt-4 font-light">
+                {card.title}
+              </p>
             </div>
-          </div>
-        </CardMateri>
-      )}
-    </div>
+            <div className="z-20 absolute top-0 flex flex-row justify-center items-end w-full h-full">
+              <div className="h-1/4">
+                <CardButton link={card.link}/>
+              </div>
+            </div>
+          </CardMateri>
+        )}
+      </div>
+    </>
   )
 }
 
