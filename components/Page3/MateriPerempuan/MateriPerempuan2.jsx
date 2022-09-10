c97a3f4b41437c40ca3e5bbff3cd37e0a3537743
rianwp <rianwp2015@gmail.com>
2022-09-10 21:21:53 +0700

    Update MateriPerempuan2.jsx

--- components/Page3/MateriPerempuan/MateriPerempuan2.jsx
+++ components/Page3/MateriPerempuan/MateriPerempuan2.jsx
@@ -3,7 +3,7 @@ const MateriPerempuan2 = () => {
     <>
       <div className="mb-8 text-white font-poppins md:text-xl text-sm font-bold">Ibu Rumah Tangga VS Wanita Karir</div>
       <div className="text-justify text-white md:indent-16 indent-8 font-poppins md:text-base sm:text-sm text-xs mb-8">
-        Tau gak sih sobat GE , kalau perempuan itu banyak dikasih pilihan. Antara menjadi ibu rumah tangga, atau menjadi wanita karir. Padahal menurut Lee, perempuan bisa melakukan dua hal tersebut tanpa harus ada satu pilihan yang wajib dipilih. Tetapi tetap saja, banyak yang bilang kalau sekolah tinggi-tinggi itu buat apa. Toh nanti ujung-ujungnya juga didapur. Sering kan ya kita mendengar hal itu ?
+        Tau gak sih sobat GE , kalau perempuan itu banyak dikasih pilihan. Antara menjadi ibu rumah tangga atau menjadi wanita karir. Padahal menurut Lee, perempuan bisa melakukan dua hal tersebut tanpa harus ada satu pilihan yang wajib dipilih. Tetapi tetap saja, banyak yang bilang kalau sekolah tinggi-tinggi itu buat apa. Nanti ujung-ujungnya juga didapur. Sering kan ya kita mendengar hal itu ?
       </div>
       <div className="text-justify text-white md:indent-16 indent-8 font-poppins md:text-base sm:text-sm text-xs mb-8">
         Sebenarnya, perempuan sekolah tinggi itu agar bisa menjadikan dirinya guru pertama yang baik untuk anaknya dan juga ia ingin  dapat menjadi pembimbing yang hebat bagi sang anak kelak. Kalaupun didapur pun, tidak masalah dengan hal itu. Karena setiap perempuan memiliki hak atas pilihan dirinya sendiri. 
