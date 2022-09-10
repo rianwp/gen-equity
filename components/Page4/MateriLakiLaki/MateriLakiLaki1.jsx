40836cc8dcec7c6a70cf1e58b0687889b4d8ce3a
rianwp <rianwp2015@gmail.com>
2022-09-10 20:06:14 +0700

    Create MateriLakiLaki1.jsx

---
+++ components/Page4/MateriLakiLaki/MateriLakiLaki1.jsx
@@ -0,0 +1,15 @@
+const MateriLakiLaki1 = () => {
+  return (
+    <>
+      <div className="mb-8 text-white font-poppins md:text-xl text-sm font-bold">Laki-Laki Tidak Boleh Menangis</div>
+      <div className="text-justify text-white md:indent-16 indent-8 font-poppins md:text-base sm:text-sm text-xs mb-8">
+        Stereotipe banyak juga dirasakan oleh laki-laki. Akan tetapi, masih kurang perhatian dalam hal tersebut. Yang pertama soal masalah laki-laki yang tidak boleh merasakan menangis.
+      </div>
+      <div className="text-justify text-white md:indent-16 indent-8 font-poppins md:text-base sm:text-sm text-xs">
+        Menangis adalah suatu hal yang dianggap tabu oleh banyak orang dikarenakan mereka beranggapan bahwasannya laki-laki harus kuat, tidak boleh menangis. Karena stereotipe tersebut malah menjadikan efek negative kepada laki-laki dimana dia dituntut untuk menjadi sosok yang kuat dan selalu tegar. Sehingga, terkadang mereka bingung dalam menyampaikan kesedihannya. Laki-laki ketika dia sedang menangis itu merupakan hal yang wajar terjadi, dengan menangis beban yang ditanggung seperti kesedihan ataupun lainnya, dia dapat menumpahkan perasaannya dan juga semua keluh kesahnya lewat tangisan. Karena pada dasarnya dia juga makhluk Tuhan yang mempunyai hak yang sama juga dengan perempuan, dia juga bisa mengeluarkan air mata. Jadi sobat GE Lee berpesan kepada kamu, jangan menghakimi teman laki-laki kamu karena dia sedang menangis ya bisa jadi dia sedang depresi atau sedih dan meluapkannya dengan tangisan. Berikan dukungan kalaupun dia menangis dia merupakan sosok laki-laki yang kuat.
+      </div>
+    </>
+  )
+}
+
+export default MateriLakiLaki1
