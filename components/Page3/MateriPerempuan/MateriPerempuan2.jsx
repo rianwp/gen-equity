6c87803ef4445aba5cd452931dd79dbcccd958e3
rianwp <rianwp2015@gmail.com>
2022-09-10 20:05:58 +0700

    Update MateriPerempuan2.jsx

--- components/Page3/MateriPerempuan/MateriPerempuan2.jsx
+++ components/Page3/MateriPerempuan/MateriPerempuan2.jsx
@@ -2,13 +2,13 @@ const MateriPerempuan2 = () => {
   return (
     <>
       <div className="mb-8 text-white font-poppins md:text-xl text-sm font-bold">Ibu Rumah Tangga VS Wanita Karir</div>
-      <div className="text-justify text-white md:indent-16 indent-8 font-poppins md:text-base sm:text-sm text-xs">
+      <div className="text-justify text-white md:indent-16 indent-8 font-poppins md:text-base sm:text-sm text-xs mb-8">
         Tau gak sih sobat GE , kalau perempuan itu banyak dikasih pilihan. Antara menjadi ibu rumah tangga, atau menjadi wanita karir. Padahal menurut Lee, perempuan bisa melakukan dua hal tersebut tanpa harus ada satu pilihan yang wajib dipilih. Tetapi tetap saja, banyak yang bilang kalau sekolah tinggi-tinggi itu buat apa. Toh nanti ujung-ujungnya juga didapur. Sering kan ya kita mendengar hal itu ?
-        <br/>
-        <br/>
+      </div>
+      <div className="text-justify text-white md:indent-16 indent-8 font-poppins md:text-base sm:text-sm text-xs mb-8">
         Sebenarnya, perempuan sekolah tinggi itu agar bisa menjadikan dirinya guru pertama yang baik untuk anaknya dan juga ia ingin  dapat menjadi pembimbing yang hebat bagi sang anak kelak. Kalaupun didapur pun, tidak masalah dengan hal itu. Karena setiap perempuan memiliki hak atas pilihan dirinya sendiri. 
-        <br/>
-        <br/>
+      </div>
+      <div className="text-justify text-white md:indent-16 indent-8 font-poppins md:text-base sm:text-sm text-xs">
         Ketika dia memutuskan untuk menjadi ibu rumah tangga itu adalah keputusan yang baik juga, tidak ada salahnya juga kan. karena ibu rumah tangga juga sama beratnya dalam bekerja. Menjadi wanita karir pun itu juga tidak masalah, karena mereka juga berhak menentukan itu untuk dirinya sendiri. Yang jelas, dia tidak melupakan apa saja yang menjadi kewajibannya. Kalaupun harus memilih keduanya, itu juga hal yang perlu diapresiasi. Karena membagi waktu diantara kedua hal itu bukanlah hal yang mudah. Jadi sobat Lee, dukung perempuan yuk agar bisa menentukan pilihannya.
       </div>
     </>
