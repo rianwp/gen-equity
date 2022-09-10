a34eca246d71de1b7096098a353217e34cd3a63b
rianwp <rianwp2015@gmail.com>
2022-09-10 23:43:56 +0700

    Update PageTransition.jsx

--- components/GlobalComponents/PageTransition.jsx
+++ components/GlobalComponents/PageTransition.jsx
@@ -4,8 +4,8 @@ import { useRouter } from "next/router"
 const PageTransition = ({children}) => {
   const router = useRouter()
   const pageAnimation = {
-    in: { opacity: 1,  transition: { duration: 0.5 } },
-    out: { opacity: 0,  transition: { duration: 0.5 } }
+    in: { opacity: 1,  transition: { duration: 0.3 } },
+    out: { opacity: 0,  transition: { duration: 0.3 } }
   }
   return (
     <AnimatePresence
