ebde44dfa427bfc3f2fbeb43aad20e788bff1a36
rianwp <rianwp2015@gmail.com>
2022-09-10 20:05:49 +0700

    Create PageTransition.jsx

---
+++ components/GlobalComponents/PageTransition.jsx
@@ -0,0 +1,28 @@
+import { motion, AnimatePresence } from "framer-motion"
+import { useRouter } from "next/router"
+
+const PageTransition = ({children}) => {
+  const router = useRouter()
+  const pageAnimation = {
+    in: { opacity: 1,  transition: { duration: 0.3 } },
+    out: { opacity: 0,  transition: { duration: 0.3 } }
+  }
+  return (
+    <AnimatePresence
+      initial={false}
+      exitBeforeEnter
+    >
+      <motion.div
+        key={router.asPath}
+        variants={pageAnimation}
+        animate="in"
+        initial="out"
+        exit="out"
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  )
+}
+
+export default PageTransition
