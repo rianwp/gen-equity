e5aecb780db6741e2123785265c02e1c3c300e50
rianwp <rianwp2015@gmail.com>
2022-09-10 20:06:05 +0700

    Create index.jsx

---
+++ components/Page4/IntroMateri3/index.jsx
@@ -0,0 +1,16 @@
+import Section from "../../GlobalComponents/Section"
+import TextIntroMateri3 from "./TextIntroMateri3"
+import TitleIntroMateri3 from "./TitleIntroMateri3"
+
+const IntroMateri3 = () => {
+  return (
+    <Section>
+      <div className="w-full h-full flex items-center justify-center flex-col">
+        <TitleIntroMateri3/>
+        <TextIntroMateri3/>
+      </div>
+    </Section>
+  )
+}
+
+export default IntroMateri3
